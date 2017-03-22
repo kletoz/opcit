a4aeb851fb0fba88eacf61135c97058a18250f8a
Kleto Zan <kletozan@br.ibm.com>
2017-03-22 08:07:36 -0300

    Processamento de argumentos da linha de comando
    Inclusão de biblitecas padrão:
    
    stdlib.h: para exit()
    string.h: para strcmp()

--- calc.c
+++ calc.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <libcalc.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("calc\n");
-    version();
+    if (argc != 2)
+    {
+        printf("Usage: %s version\n", argv[0]);
+        exit(1);
+    }
+
+    if (strcmp(argv[1], "version") == 0) 
+        version();
+    else
+        printf("Usage: %s version\n", argv[0]);
+    
     return 0;
 }
