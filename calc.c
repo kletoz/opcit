248cf5f44453a75ffcce780377b8484fbbf2c8df
Kleto Zan <kletozan@br.ibm.com>
2017-03-22 08:10:26 -0300

    Criação função auxiliar usage()

--- calc.c
+++ calc.c
@@ -3,19 +3,23 @@
 #include <string.h>
 #include <libcalc.h>
 
+void
+usage(char *argv0)
+{
+    printf("Usage: %s version\n", argv0);
+    exit(1);
+}
+
 int
 main(int argc, char *argv[])
 {
     if (argc != 2)
-    {
-        printf("Usage: %s version\n", argv[0]);
-        exit(1);
-    }
+        usage(argv[0]);
 
     if (strcmp(argv[1], "version") == 0) 
         version();
     else
-        printf("Usage: %s version\n", argv[0]);
+        usage(argv[0]);
     
     return 0;
 }
