77135dc6ebbcd8e2bbc366a9b8fd8b3db5936231
Kleto Zan <kletozan@br.ibm.com>
2017-03-23 13:38:50 -0300

    Refactoring: Separação das funções de params em novo arquivo
    As função params_split() é útil também para separar os argumentos da linha lida
    em op_load(). Por isso, essas funções estão num arquivo separado de opcit.c.

--- src/opcit.c
+++ src/opcit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "calc.h"
-#include "util.h"
+#include "params.h"
 
 void
 usage(char *argv0)
@@ -19,90 +19,6 @@ usage(char *argv0)
     printf("Other operators:\n");
     printf("    fibo\n");
     exit(1);
-}
-
-char **
-params_split(char *text, int text_len, int *params_num)
-{
-    char *s, *token, *copy, **params;
-    int len, n;
-    struct list *list = NULL, *node;
-
-    for (s = text, n = 0; /* nothing */; s = NULL)
-    {
-        token = strtok(s, " \t\n");
-
-        if (token == NULL)
-            break;
-
-        len = strlen(token);
-
-        if (token[len - 1] == '\n')
-            token[--len] = '\0';
-
-        if (len == 0)
-            continue;
-
-        copy = strdup(token);
-        list = list_prepend(list, copy);
-        n++;
-    }
-
-    params = malloc(n * sizeof(*params));
-    *params_num = n;
-
-    for (node = list; node; node = node->next)
-        params[--n] = (char *) node->data;
-
-    list_destroy(list);
-
-    return params;
-}
-
-char **
-params_read(int *params_num)
-{
-#define BUFSIZE 3
-
-    char *s, *input, **params, buf[BUFSIZE];
-    int input_size, input_length, slen, all_read = 0;
-
-    input_length = 0;
-    input_size = 10;
-    input = malloc(input_size * sizeof(*input));
-
-    do
-    {
-        s = fgets(buf, BUFSIZE, stdin);
-        all_read = feof(stdin);
-
-        if (s == NULL)
-            break;
-
-        slen = strlen(s);
-
-        if (slen + input_length > input_size)
-        {
-            input_size += slen + 10;
-            input = realloc(input, input_size * sizeof(*input));
-        }
-
-        strcpy(input + input_length, buf);
-        input_length += slen;
-    } while (!all_read);
-
-    params = params_split(input, input_length, params_num);
-
-    free(input);
-
-    return params;
-}
-
-void
-params_destroy(char **params, int params_num)
-{
-    for (params_num--; params_num >= 0; params_num--)
-        free(params[params_num]);
 }
 
 int
---
+++ src/params.h
@@ -0,0 +1,3 @@
+extern char **params_split(char *, int, int *);
+extern char **params_read(int *);
+extern void params_destroy(char **, int);
