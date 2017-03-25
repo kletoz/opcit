acb562f5903b26b7eed0ee84faac5338410c0611
Kleto Zan <kletozan@br.ibm.com>
2017-03-25 18:01:39 -0300

    Implementação de semáforos para a shared memory
    Controle de acesso a shared memory com semáforos readers/writers.
    
    Garantimos que a shared memory será acessada por um único escritor a cada vez e
    leitores simultanemanete.
    
    Durante a escrita os leitores não terão acesso, aguardando a escrita.

--- src/calc.c
+++ src/calc.c
@@ -4,8 +4,8 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include "params.h"
 
 void
@@ -179,6 +179,8 @@ op_mulv(char *a, char *b)
     op_vector(a, b, op_vector_mul);
 }
 
+#define SHM_FILE_KEY ".shmkey"
+
 /* Início de um segmento de shared memory System V.
  *
  * Existem duas bibliotecas diferentes para shared memory: System V e POSIX.
@@ -190,8 +192,6 @@ op_mulv(char *a, char *b)
 void *
 shm_init(size_t size, int flag)
 {
-#define SHM_FILE_KEY ".shmkey"
-
     key_t key;
     FILE *filekey;
     void *shm;
@@ -240,6 +240,233 @@ shm_init(size_t size, int flag)
     return shm;
 }
 
+#define SEM_NUM 2
+
+enum sem
+{
+    SEM_READER,
+    SEM_WRITER
+};
+
+/* Início de um conjunto de semáforos.
+ *
+ * Os semafóros são usados para o controle de acesso a shared memory.
+ *
+ * Temos dois tipos processos que podem acessar a shared memory:
+ *
+ * opcit load <arquivo>
+ * opcit search <string>
+ *
+ * O processo load é de escrita e o processo search é de leitura exclusivamente.
+ * Apesar de termos dois tipos de processos, podemos ter vários processos
+ * executando ao mesmo tempo usando a shared memory. Por exemplo, podemos
+ * executar duas ou mais cargas simultaneamente e também várias buscas ao mesmo
+ * tempo.
+ *
+ * Vamos seguir a regra básica de semáforos:
+ *
+ * 1) o acesso de escrita deve ser exclusivo (apenas um escritor por vez)
+ * 2) o acesso de escrita bloqueia todas as leituras
+ * 3) as leituras podem acontecer todas ao mesmo tempo, sem bloqueio
+ * 4) a prioridade é de escrita
+ *
+ * São criados dois semáforos: uma para controlar a escrita e outro a leitura.
+ * Os dois semáforos estão no mesmo segmento. */
+int
+sem_init(int role)
+{
+    int i, id, flag, already_exists;
+    key_t key;
+    FILE *filekey;
+    unsigned short semun_array[SEM_NUM];
+
+    /* Cria um arquivo se não existir para ser usado como chave dos semáforos.
+     * Convenientemente estamos usando o mesmo arquivo do segmento de shared
+     * memory. */
+    filekey = fopen(SHM_FILE_KEY, "a");
+    fclose(filekey);
+
+    key = ftok(SHM_FILE_KEY, 'x');
+
+    if (key == -1)
+    {
+        printf("ftok(): %s: %s\n", SHM_FILE_KEY, strerror(errno));
+        exit(1);
+    }
+
+    /* Se o processo for de escrita, usar as flags de criação exclusiva. Ou
+     * seja, o conjunto de semáforos só será criado se não existir. */ 
+    if (role == SEM_WRITER)
+        flag = IPC_CREAT | IPC_EXCL | 0660;
+    else
+        flag = 0660;
+
+    already_exists = 0;
+    id = semget(key, SEM_NUM, flag);
+
+    /* Se o processo for de escrita e semget() não conseguiu criar os semáforos
+     * exclusivamente (ou seja, eles já existem), obtém o id dos semáforos já
+     * existentes. */
+    if (id == -1 && role == SEM_WRITER && errno == EEXIST)
+    {
+        already_exists = 1;
+        flag = 0660;
+        id = semget(key, SEM_NUM, flag);
+    }
+    
+    if (id == -1)
+    {
+        printf("semget(): %s: %d: %s\n", SHM_FILE_KEY, key,
+               strerror(errno));
+        exit(1);
+    }
+
+    /* Caso o processo seja de escrita e a criação dos semáforos aconteceu nesta
+     * execução, isto é, os semáforos ainda não existiam e foram criados agora,
+     * definir todos os semáforos do conjunto (2 neste caso) como 0. Zero
+     * significa que o processo pode continuar. Dessa forma, o primeiro processo
+     * que acessar o semáforo poderá continuar.
+     *
+     * Os acessos seguintes que sejam de escrita (role SEM_WRITER) não podem
+     * alterar o valor dos semáforos para zero novamente, ou todo o controle
+     * seria perdido. */
+    if (role == SEM_WRITER && !already_exists)
+    { 
+        for (i = 0; i < SEM_NUM; i++)
+            semun_array[i] = 0;
+
+        if (semctl(id, 1, SETALL, semun_array) == -1)
+        {
+            printf("semctl(): %s: %d: %s\n", SHM_FILE_KEY, key,
+                   strerror(errno));
+            exit(1);
+        }
+    }
+
+    return id;
+}
+
+void
+table_write_lock(int semid)
+{
+    struct sembuf ops[3];
+
+    /* Para obter o lock de escrita, são feitas três operações nos 2 semáforos.
+     * 
+     * A primeira operação é sem_op 0, que significa aguardar até que o semáforo
+     * de escrita esteja livre. Estamos definindo o semáforo de escrita como o
+     * que tem sem_num igual a SEM_WRITER -- ou 1, de acordo com nossa enum sem.
+     *
+     * Quando o semáforo SEM_WRITER estiver liberado (atingir zero), nos fazemos
+     * uma outra operação, que é a sem_op 1 no semáforo SEM_WRITER. Esta
+     * operação no semáforo trava o semáforo para outro processo que chegar
+     * depois desse.
+     *
+     * Além de travar o semáforo de escrita SEM_WRITER, fazemos mais uma
+     * operação, que é aguardar o semáforo de leitura SEM_READER (ou 0 na enum
+     * sem) ser liberado. Ou seja, antes de escrever vamos aguardar todos os
+     * processos que estiverem escrevendo terminar e *também* aguardar todos os
+     * processos que estiverem lendo terminar. 
+     *
+     * Quando todos os processos de leitura tiverem terminado (operação 3,
+     * ops[2]), além dos de escrita (operação 1, ops[0]), podemos seguir com a
+     * ação de escrita na memória. */
+    ops[0].sem_num = SEM_WRITER;
+    ops[0].sem_op = 0;
+    ops[0].sem_flg = SEM_UNDO;
+    
+    ops[1].sem_num = SEM_WRITER;
+    ops[1].sem_op = 1;
+    ops[1].sem_flg = SEM_UNDO;
+    
+    ops[2].sem_num = SEM_READER;
+    ops[2].sem_op = 0;
+    ops[2].sem_flg = SEM_UNDO;
+
+    if (semop(semid, ops, 3) == -1)
+    {
+        printf("semopget(): %s: write lock: %s\n", SHM_FILE_KEY,
+               strerror(errno));
+        exit(1);
+    }
+}
+
+void
+table_write_unlock(int semid)
+{
+    struct sembuf ops[1];
+   
+    /* A liberação de um lock de escrita é mais simples. Apenas uma operação é
+     * necessária em um único semáforo.
+     *
+     * A operação é liberar o semáforo, colocando sem_op -1.
+     *
+     * Estamos usando -1 aqui porque usamos sem_op 1 no lock. Mas pode ser usado
+     * qualquer inteiro, desde que seja o mesmo em módulo. */
+    ops[0].sem_num = SEM_WRITER;
+    ops[0].sem_op = -1;
+    ops[0].sem_flg = SEM_UNDO;
+    
+    if (semop(semid, ops, 1) == -1)
+    {
+        printf("semopget(): %s: write unlock: %s\n", SHM_FILE_KEY,
+               strerror(errno));
+        exit(1);
+    }
+}
+
+void
+table_read_lock(int semid)
+{
+    struct sembuf ops[2];
+
+    /* Para obter o lock de leitura fazemos duas operações em dois semáforos.
+     *
+     * Primeira operação é aguardar o semáforo de escrita ser liberado. Está é a
+     * sem_op 0.
+     *
+     * Quando o semáforo de escrita for zero, podemos incrementar o semáforo de
+     * leitura com sem_op 1.
+     *
+     * Não precisamos aguardar a fila de leitores pois o acesso de leitura pode
+     * ser concorrente. O semáforo SEM_READER e incrementado para avisar para o
+     * próximo escritor que estamos ocupando o recurso controlado pelo semáforo
+     * (shared memory, no caso). */
+    ops[0].sem_num = SEM_WRITER;
+    ops[0].sem_op = 0;
+    ops[0].sem_flg = SEM_UNDO;
+    
+    ops[1].sem_num = SEM_READER;
+    ops[1].sem_op = 1;
+    ops[1].sem_flg = SEM_UNDO;
+    
+    if (semop(semid, ops, 2) == -1)
+    {
+        printf("semopget(): %s: read lock: %s\n", SHM_FILE_KEY,
+               strerror(errno));
+        exit(1);
+    }
+}
+
+void
+table_read_unlock(int semid)
+{
+    struct sembuf ops[1];
+   
+    /* A liberação de um lock de leitura é apenas decrementar o valor do
+     * semáforo de leitura. */ 
+    ops[0].sem_num = SEM_READER;
+    ops[0].sem_op = -1;
+    ops[0].sem_flg = SEM_UNDO;
+    
+    if (semop(semid, ops, 1) == -1)
+    {
+        printf("semopget(): %s: read unlock: %s\n", SHM_FILE_KEY,
+               strerror(errno));
+        exit(1);
+    }
+}
+
 #define CONTACTS_NUM 10
 #define CONTACTS_NAME_SIZE 50
 #define CONTACTS_EMAIL_SIZE 50
@@ -256,7 +483,7 @@ op_load(char *filename)
 #define BUFSIZE 10
 
     char *s, *input, *line, **params, buf[BUFSIZE];
-    int k, *kp, slen, lineno, input_length, input_size, params_num;
+    int k, *kp, slen, lineno, input_length, input_size, params_num, sem;
     void *shm;
     struct contacts *contacts;
     FILE *file = fopen(filename, "r");
@@ -268,6 +495,8 @@ op_load(char *filename)
     }
 
     shm = shm_init(CONTACTS_NUM * sizeof(*contacts), IPC_CREAT);
+    sem = sem_init(SEM_WRITER);
+    
     kp = (int *) shm;
     contacts = (struct contacts *) (shm + sizeof(*kp));
     k = 0;
@@ -276,6 +505,8 @@ op_load(char *filename)
     input_size = 10;
     input = malloc(input_size * sizeof(*input));
     lineno = 0;
+    
+    table_write_lock(sem);
 
     while ((s = fgets(buf, BUFSIZE, file)))
     {
@@ -363,7 +594,9 @@ op_load(char *filename)
             params_destroy(params, params_num);
         }
     }
-    
+
+    table_write_unlock(sem);
+
     *kp = k;
 
     if (ferror(file))
@@ -379,12 +612,15 @@ op_load(char *filename)
 void
 op_search(char *token)
 {
-    int i, k;
+    int i, k, sem;
     void *shm;
     struct contacts *contacts;
     
     shm = shm_init(CONTACTS_NUM * sizeof(*contacts), 0);
+    sem = sem_init(SEM_READER);
   
+    table_read_lock(sem);
+
     /* Lê o início do segmento de shared memory como um inteiro.
      * O ponteiro para o segmento de memório é do tipo void *, ou seja, não tem
      * um tipo específico, é só memória.
@@ -413,4 +649,6 @@ op_search(char *token)
     if (strcmp(token, "all") == 0)
         for (i = 0; i < k; i++)
             printf("%-50s | %-50s\n", contacts[i].name, contacts[i].email);
+    
+    table_read_unlock(sem);
 }
